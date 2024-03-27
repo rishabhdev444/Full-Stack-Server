f14f674be9b35cd9af6ed19beeec4ad5063c7250
RISHABH DEV <53978955+rishabhdev444@users.noreply.github.com>
2024-03-26 23:44:25 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
